b4363ea06a247ef98212974716e44c10b4c3cfe3
Thando <thandwem045@gmail.com>
2025-07-21 23:23:23 +0200

    Added styling and functionality for hamburger div

--- scripts.js
+++ scripts.js
@@ -32,3 +32,26 @@ careers.click(() => footerAnimation(careers, careersModal));
 business.click(() => footerAnimation(business, businessModal));
 social.click(() => footerAnimation(social, socialModal));
 pick.click(() => footerAnimation(pick, pickModal));
+
+let modalMain = $(".mobile-div")
+let hamburger = $(".hamburger")
+let isHamModalOpen = false
+let overlay = $(".overlay")
+
+function hamModal () {
+     if (!isHamModalOpen) {
+       isHamModalOpen = true;
+         modalMain.css("display", "flex")
+         $("body").addClass("no-scroll")
+         overlay.css("display", "block");
+         
+     } else {
+        isHamModalOpen = false;
+         modalMain.css("display", "none")
+         $("body").removeClass("no-scroll")
+          overlay.css("display", "none")
+        
+     }
+}
+
+hamburger.click(() => hamModal())
